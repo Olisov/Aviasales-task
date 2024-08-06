707d824ec7bb0e1cf12e442a51a93c1a9dc910a2
Denis OFox <olisow.denis@yandex.ru>
2024-08-06 18:12:36 +0300

    app first start

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import { React } from 'react'
+import './app.scss'
+
+function App() {
+  return (
+    <>
+      <div className="hi">Hi Aviasales</div>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+    </>
+  )
+}
+
+export default App
